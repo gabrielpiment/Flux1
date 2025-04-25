301cef9360ea4bb5874628e8896212f0239e3bae
Gabriel Pimenta Freitas Félix <biel.pimenta@hotmail.com>
2025-04-25 15:33:24 -0300

    Configuração do GitHub Pages para o repositório Flux1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/flux-crm/",
+  base: "/Flux1/",
   server: {
     host: "::",
     port: 8080,
